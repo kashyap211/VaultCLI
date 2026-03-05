758939c135f69cc9e2a940a5bc41a5a6af882a04
kashyap211 <kashyapkashyap211@gmail.com>
2026-03-05 10:41:42 +0530

    builded account.cpp

--- account.cpp
+++ account.cpp
@@ -13,8 +13,8 @@ public:
     Account();
     Account(string s, string u, string p);
 
-    string toFileString();      // convert to: site|user|pass
-    static Account fromLine(string line);  // parse from file line
+    string toFileString();     
+    static Account fromLine(string line);  
 };
 
 #endif
